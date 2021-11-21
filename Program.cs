9dca9919cbe871547a500278581ea6cc51d381e3
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-21 16:22:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fno = 0,sno=1,res=0;
+            Console.WriteLine("Enter number till you want to print fibbonacci series");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            Console.Write(fno+" "+sno);
+            for (int i = 2; i < ch; i++)            
+            {
+                res = fno + sno;
+                Console.Write(res+" ");
+                fno = sno;
+                sno = res;
+            }
+        }
+    }
+}
