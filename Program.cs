21ad08f7fe1e2cb2392d7c1f4ec650cb084f6d17
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-21 16:25:41 +0530

    Add files via upload
    Switch Case Program

--- Program.cs
+++ Program.cs
@@ -1,22 +1,55 @@
 ﻿using System;
 
-namespace fibonacci
+namespace switch1
 {
     class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int fno = 0,sno=1,res=0;
-            Console.WriteLine("Enter number till you want to print fibbonacci series");
-            int ch = Convert.ToInt32(Console.ReadLine());
-            Console.Write(fno+" "+sno);
-            for (int i = 2; i < ch; i++)            
+        Console.WriteLine("Enter number of month 1-12 to find out days of it");
+        int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
             {
-                res = fno + sno;
-                Console.Write(res+" ");
-                fno = sno;
-                sno = res;
+                case 1:
+                    Console.WriteLine("31 Days");
+                    break;
+                case 2:
+                    Console.WriteLine("28 days");
+                    break;
+                case 3:
+                    Console.WriteLine("31 days");
+                    break;
+                case 4:
+                    Console.WriteLine("30 days");
+                    break;
+                case 5:
+                    Console.WriteLine("31 days");
+                    break;
+                case 6:
+                    Console.WriteLine("30 days");
+                    break;
+                case 7:
+                    Console.WriteLine("31 days");
+                    break;
+                case 8:
+                    Console.WriteLine("31 days");
+                    break;
+                case 9:
+                    Console.WriteLine("31 days");
+                    break;
+                case 10:
+                    Console.WriteLine("30 days");
+                    break;
+                case 11:
+                    Console.WriteLine("30 days");
+                    break;
+                case 12:
+                    Console.WriteLine("31 days");
+                    break;
+                default:
+                    Console.WriteLine("Enter correct number");
+                    break;
             }
         }
-    }
+}
 }
