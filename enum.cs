c376539c0765c0dc9b8b773d1f350aa4e59c7478
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-21 22:41:32 +0530

    Add files via upload

---
+++ enum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Assignment5
+{
+    class Employe
+    {
+        public string name;
+        public int age;
+        public Employe(int age, string name)
+        {
+            this.name = name;
+            this.age = age;
+
+        }
+    }
+    class permanent :Employe
+    {
+        public int emp_id;
+        public int emp_salary;
+
+        public permanent(int id, int salary, string name, int age) : base(age, name)
+        {
+            emp_id = id;
+            emp_salary = salary;
+        }
+        public void PrintEmployee()
+        {
+            Console.WriteLine("Emp ID:      " + emp_id);
+            Console.WriteLine("Emp Name:    " + name);
+            Console.WriteLine("Emp Salary:  " + emp_salary);
+            Console.WriteLine("Emp Age:     " + age);
+            Console.WriteLine("\n\n");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            permanent E = new permanent(101, 70000, "Hrishi", 24);
+            E.PrintEmployee();
+        }
+    }
+}
