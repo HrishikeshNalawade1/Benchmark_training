5154ff9ed37c572aec76484e94a196a2a2c3b8aa
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-21 22:44:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment7
+{
+    enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thrusday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int myDay = (int)Days.Saturday;
+            Console.WriteLine(myDay);
+        }
+    
+    }
+}
