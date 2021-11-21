54302511e34ffddc33234fb88dee67f54015aff2
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-21 16:41:20 +0530

    Add files via upload

---
+++ assign5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nam,desgi;
+            int sal;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("\nEnter name of employee");
+                 nam = Console.ReadLine();
+                Console.WriteLine("Enter salary of employee");
+                 sal = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Designation of Employee");
+                 desgi = Console.ReadLine();
+                Console.WriteLine("\n"+nam+" "+sal+" "+desgi);
+            }
+            
+        }
+    }
+}
