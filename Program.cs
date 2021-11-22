29e462a0d0853d6f3f7fd285af08936c7e93f85c
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-22 14:02:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace seal
+{
+   // sealed
+    class parent
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("This is parent");
+        }
+    }
+    class child:parent
+    {
+        public override void  show ()
+        {
+            Console.WriteLine("This is child");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            parent p = new child();
+            p.show();
+        }
+    }
+}
