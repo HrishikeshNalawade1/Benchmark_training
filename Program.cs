8d5b7721d5b5f49fd514dd515554554c03084276
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-22 14:14:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace inter
+{
+    interface iprintable
+    {
+        public void show();
+    }
+    class Employe:iprintable
+    {
+        public void show()
+        {
+            Console.WriteLine("This is Employee");
+        }
+    }
+    class Account:iprintable
+    {
+        public void show()
+        {
+            Console.WriteLine("This is account");
+        }
+    }
+    class Program
+    {
+        public void CallIprintable(iprintable p)
+        {
+            p.show();
+        }
+        static void Main(string[] args)
+        {
+            Account a = new Account();
+            //a.show();
+            Employe e = new Employe();
+            // e.show();
+            Program p = new Program();
+            p.CallIprintable(a);
+            p.CallIprintable(e);
+        }
+    }
+}
