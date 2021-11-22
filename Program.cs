dfdcd23e22effe4975ff0dc2c317fa5d2578ad6b
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-22 14:06:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two number");
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                int res = num / num1;
+                Console.WriteLine(+res);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
