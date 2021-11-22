2ad6f0ba5921230d474527348eb51eca6557ee06
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-22 14:05:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exec1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            Console.WriteLine("Enter elements in array");
+            for (int i = 0; i < 7; i++)
+            {
+                try
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                    
+                }
+            }
+        }
+    }
+}
