a9b09968639cadecd5d897bb3fa2b1cc02d0c2cd
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-24 06:33:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿
+    using System;
+    using System.Xml.Serialization;
+    using System.IO;
+
+    namespace serialization
+    {
+        namespace Serialize
+        {
+            [XmlRoot(ElementName = "Employee")]
+            public class Employee
+            {
+                private string empID;
+                private string empName;
+                private int empAge;
+                private string[] empskill;
+                [XmlAttribute(AttributeName = "EmpID")]
+                public string EmpID
+                {
+                    get { return empID; }
+                    set { empID = value; }
+                }
+                [XmlElement(ElementName = "name")]
+                public string Name
+                {
+                    get { return empName; }
+                    set { empName = value; }
+                }
+                [XmlElement(ElementName = "Age")]
+                public int Age
+                {
+                    get { return empAge; }
+                    set { empAge = value; }
+                }
+                [XmlElement(ElementName = "Skill")]
+                public string[] Skill
+                {
+                    get { return empskill; }
+                    set { empskill = value; }
+                }
+            }
+
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    Employee SerializeEmp = new Employee();
+                    string[] empskills = { "c#", "SQL server 2000" };
+                    SerializeEmp.EmpID = "emp1";
+                    SerializeEmp.Name = "hrishi";
+                    SerializeEmp.Age = 23;
+                    SerializeEmp.Skill = empskills;
+
+                    StreamWriter newStream = File.CreateText(@"C:\Users\Hrishikesh\source\samplew.xml");
+                    XmlSerializer xmlSerialize = new XmlSerializer(typeof(Employee));
+
+                    xmlSerialize.Serialize(newStream, SerializeEmp);
+                    newStream.Close();
+                    Employee deserializeEmp = new Employee();
+
+
+                    StreamReader readStream = File.OpenText(@"C:\Users\Hrishikesh\source\samplew.xml");
+                    xmlSerialize = new XmlSerializer(typeof(Employee));
+
+
+                    deserializeEmp = (Employee)xmlSerialize.Deserialize(readStream);
+                    readStream.Close();
+                    Console.WriteLine("Deserialize object :" + deserializeEmp.Name);
+                    Console.WriteLine("deserialize object:" + SerializeEmp.Name);
+
+                }
+            }
+        }
+    }
+
+
+
+
