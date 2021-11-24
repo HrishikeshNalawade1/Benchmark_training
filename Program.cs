57ca80de5206b1d35f923572d5f09fb6836abc19
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-24 06:31:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tostr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021,7,11);
+            Console.WriteLine(dt.ToString());
+        }
+    }
+}
