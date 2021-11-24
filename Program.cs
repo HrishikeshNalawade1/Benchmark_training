d9bb0e9b96aebec923bc54665ed9c383c2c34bac
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-24 06:34:08 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+namespace fileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo("C:\\Users\\Hrishikesh\\source\\hii");
+            if(di.Exists)
+            {
+                Console.WriteLine(di.FullName);
+                Console.WriteLine(di.CreationTime);
+                Console.WriteLine(di.Parent);
+                Console.WriteLine(di.LastWriteTime);
+                FileInfo[] fi = di.GetFiles();
+                foreach(FileInfo f in fi)
+                {
+                    Console.WriteLine(f.FullName);
+                    Console.WriteLine(f.Extension);
+                    Console.WriteLine(f.CreationTime);
+                    Console.WriteLine(f.Length);
+                    Console.WriteLine("*******");
+                }
+            }
+            else
+            {
+                di.Create();
+                Console.WriteLine("Directory Creadted");
+            }
+        }
+    }
+}
