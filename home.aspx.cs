dee2c6ad65e8e76237da3ffc85962de32f8a3c01
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-27 17:32:53 +0530

    Add files via upload

---
+++ home.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+public partial class home : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnlogin_Click(object sender, EventArgs e)
+    {
+        if(Txtlogin.Text=="")
+        {
+            lblerr.ForeColor = Color.Black;
+            lblerr.Text = "Please Enter Login Name";
+        }
+        else if(Txtpass.Text=="")
+        {
+            lblpass.Text = "Please enter Password";
+        }
+        else
+        {
+            string LoginId = Txtlogin.Text;
+            string Password = Txtpass.Text;
+            DAL d = new DAL();
+
+            if(d.LoginId(LoginId, Password))
+            {
+                Response.Redirect("FirstForm.aspx");
+            }
+            else
+            {
+                lblerr2.ForeColor =Color.Red;
+                lblerr2.Text = "Invalid User  name or password";
+            }
+        }
+    }
+}
