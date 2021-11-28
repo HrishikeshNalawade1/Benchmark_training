63a0647d6377557b0e82c8c8ee2bb91efb5790d9
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-11-28 19:33:03 +0530

    Update and rename Default.aspx.cs to Firstpg.aspx.cs

--- Firstpg.aspx.cs
+++ Firstpg.aspx.cs
@@ -14,16 +14,6 @@ public partial class _Default : System.Web.UI.Page
 
     }
 
-    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        
-    }
-
-    protected void TextBox10_TextChanged(object sender, EventArgs e)
-    {
-
-    }
-
     protected void Button1_Click(object sender, EventArgs e)
     {
         string Fname = txtFnme.Text;
