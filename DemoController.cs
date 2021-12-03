5f74cc8c985fdc6c53e92f5bb190c44b16b67dcb
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-12-03 18:29:24 +0530

    Add files via upload

---
+++ DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class DemoController : ApiController
+    {
+       public string get()
+        {
+            return "Welcome to Web Api";
+        }
+      
+        /*public List<string> get()
+        {
+            List<string> datalist = new List<string>();
+            datalist.Add("Data1");
+            datalist.Add("Data1");
+            return datalist;
+        }*/
+        
+    }
+}
