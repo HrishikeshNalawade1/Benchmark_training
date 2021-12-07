3d833fad424a2ba21c8b224fa21bb094afecb22b
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-12-07 23:18:58 +0530

    Add files via upload

---
+++ opportunity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models
+{
+    public class opportunity
+    {
+        public int Id { get; set; }
+        public string PlanNm { get; set; }
+        public int Price { get; set; }
+        public int ConvertedBy { get; set; }
+        public string Remark { get; set; }
+        
+    }
+}
