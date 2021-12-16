c12758cf1515cf89e0e26c6db8e00119bb7b6f73
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-12-16 18:32:15 +0530

    Add files via upload

---
+++ Caculate.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.Area = void 0;
+var Area = /** @class */ (function () {
+    function Area() {
+    }
+    Area.prototype.claculateArea = function () {
+        console.log("This is for Area Calculator");
+    };
+    return Area;
+}());
+exports.Area = Area;
+//
